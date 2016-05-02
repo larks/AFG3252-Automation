87c1703c4644876ffba4e9fd010c856eaa5e28e2
Thomas Anderson <tba9h@localhost.localdomain>
2016-05-02 12:08:36 -0400

    updated afgtest

--- afgtest.py
+++ afgtest.py
@@ -1,6 +1,6 @@
 from AFG3252 import * 
 import time
-
+from 
 
 
 
@@ -12,7 +12,7 @@ dev = AFG3252('128.143.196.225')
 dev.outputType('square')
 dev.setFrequency(1500)
 dev.enableOutput(1)
-dev.sweep(10000, 50000, 100, 0.1)
+
 
 
 dev.disableOutput(1)
